2eb066b339b78e47a03be29ed93ac67e5344e950
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-04-15 22:26:58 -0500

    added multiply and isPrime functions to be exported

---
+++ utils.js
@@ -0,0 +1,13 @@
+function multiply(a, b) {
+  return a * b;
+}
+
+function isPrime(number) {
+  if (number <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+}
+
+module.exports = { multiply, isPrime };
